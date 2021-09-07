523f76a5d3608392c51228b2377deec810d4cfae
Alejandro <sanchezalejss@gmail.com>
2021-09-07 04:25:50 -0600

    Create first Component and configuration

---
+++ src/components/GifExpertApp/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+export const GifExpertApp = () => {
+    return (
+        <>
+          <h2>Gif Expert App</h2>
+          <hr/>  
+        </>
+    )
+}
