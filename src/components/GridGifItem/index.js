e9f8fd71494348a70227fd862e3351daf9403944
Alejandro <sanchezalejss@gmail.com>
2021-09-12 00:09:35 -0600

    Add lib animation.css

--- src/components/GridGifItem/index.js
+++ src/components/GridGifItem/index.js
@@ -3,7 +3,7 @@ import './style.css'
 
 export const GridGifItem = ({ id, title, url }) => {
     return (
-        <div className="card">
+        <div className="card animate__animated animate__swing">
             <img src={url} alt={title} />
             <p>{title}</p>
         </div>
