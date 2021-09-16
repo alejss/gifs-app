060c038cfdb683d96a27f59f6f71bd2364338281
Alejandro <sanchezalejss@gmail.com>
2021-09-16 03:56:17 -0600

    Props required GifGridItem Component

--- src/components/GridGifItem/index.js
+++ src/components/GridGifItem/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './style.css'
 
 export const GridGifItem = ({ id, title, url }) => {
@@ -8,4 +9,9 @@ export const GridGifItem = ({ id, title, url }) => {
             <p>{title}</p>
         </div>
     )
+}
+
+GridGifItem.prototype = {
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
 }
