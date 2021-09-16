a7930c8489ba51992dfd8b637631999611ff45f1
Alejandro <sanchezalejss@gmail.com>
2021-09-16 04:08:55 -0600

    Add test GridGifItem

--- src/tests/GifGridItem.test.js
+++ src/tests/GifGridItem.test.js
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme'
 
-import {GridGifItem} from '../components/GridGifItem'
+import { GridGifItem } from '../components/GridGifItem'
 
 describe('GifGridItem', () => {
     const data = { title: 'Testing Component', url: 'www.1337.com/hello.png' }
@@ -10,4 +10,23 @@ describe('GifGridItem', () => {
     test('Testing Snapshot Component', () => {
         expect(wrapper).toMatchSnapshot()
     })
+
+    test('The p tag must have a value with the title parameter', () => {
+        const p = wrapper.find('p')
+        expect(p.text().trim()).toBe(data.title)
+    })
+
+    test('the img tag must be equal to the url and alt of the props', () => {
+        const img = wrapper.find('img')
+        expect(img.prop('src')).toBe(data.url)
+        expect(img.prop('alt')).toBe(data.title)
+    })
+
+    test('Contain class animate__animated animate__swing', () => {
+        const div = wrapper.find('div')
+        const className = div.prop('className')
+        expect(className.includes('animate__animated animate__swing')).toBe(true)
+    })
+    
+    
 });
