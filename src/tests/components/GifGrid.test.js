3682ebc786153f2a1d457ed0424d538ebf437e7e
Alejandro <sanchezalejss@gmail.com>
2021-12-07 02:33:01 -0600

    Add testing and Snapshot GifGrid

---
+++ src/tests/components/GifGrid.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+
+import { shallow } from 'enzyme';
+import { GifGrid } from '../../components/GifGrid';
+import { useFetchGifs } from '../../hooks/useFetchGifs';
+jest.mock('../../hooks/useFetchGifs');
+
+describe('Testing <GifGrid />', () => {
+
+    const category = 'Dogs';
+
+    test('Testing Snapshot Component', () => {
+
+        useFetchGifs.mockReturnValue({
+            data: [],
+            loading: true
+        });
+
+        const wrapper = shallow( <GifGrid category={ category } /> );
+        expect( wrapper ).toMatchSnapshot();
+        
+    }); 
+
+})
+
